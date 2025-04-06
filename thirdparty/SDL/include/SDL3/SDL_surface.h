171d42e76a9aa03b5eadaa8f82243f9ee23c045b
TheGabmeister <thegabmeister@protonmail.com>
2025-04-06 20:10:09 +0800

    Replace SDL with pre-compiled binaries

--- thirdparty/SDL/include/SDL3/SDL_surface.h
+++ thirdparty/SDL/include/SDL3/SDL_surface.h
@@ -83,9 +83,8 @@ typedef Uint32 SDL_SurfaceFlags;
 typedef enum SDL_ScaleMode
 {
     SDL_SCALEMODE_INVALID = -1,
-    SDL_SCALEMODE_NEAREST,  /**< nearest pixel sampling */
-    SDL_SCALEMODE_LINEAR,   /**< linear filtering */
-    SDL_SCALEMODE_PIXELART  /**< nearest pixel sampling with improved scaling for pixel art */
+    SDL_SCALEMODE_NEAREST, /**< nearest pixel sampling */
+    SDL_SCALEMODE_LINEAR   /**< linear filtering */
 } SDL_ScaleMode;
 
 /**
