a6faf76b8e7810529eb9d6c395fadcbbf461124a
TheGabmeister <thegabmeister@protonmail.com>
2025-04-06 10:04:09 +0800

    Add SDL3

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
+#include <SDL3/SDL.h>
 
 int main()
 {
+	SDL_Init(SDL_INIT_VIDEO);
 	std::cout << "Hello CMake." << std::endl;
 	return 0;
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+Dependencies:
+- SDL 3.2.10 https://github.com/libsdl-org/SDL/releases/tag/release-3.2.10
