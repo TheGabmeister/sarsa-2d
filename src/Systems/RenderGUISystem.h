4314696623303453ca5223482510e3eeba132c83
TheGabmeister <thegabmeister@protonmail.com>
2025-04-29 18:47:27 +0800

    Edit colors

--- src/Systems/RenderGUISystem.h
+++ src/Systems/RenderGUISystem.h
@@ -42,27 +42,31 @@ class RenderGUISystem: public System {
             }
 
             // Change style
-            style.FrameRounding = 12.0f;
+            style.FrameRounding = 6.0f;
             style.GrabRounding = 12.0f;
             style.TabBorderSize = 1.0f;
             style.WindowMenuButtonPosition = ImGuiDir_None;
 
-            colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+            colors[ImGuiCol_Text] = ImVec4{ 0.9f, 0.9f, 0.9f, 1.0f };
+
+            colors[ImGuiCol_WindowBg] = ImVec4{ 0.141f, 0.161f, 0.18f, 1.0f };
 
             // Headers
-            colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+            colors[ImGuiCol_Header] = ImVec4{ 0.267f, 0.302f, 0.337f, 1.0f };
             colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
             colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
 
             // Buttons
-            colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+            colors[ImGuiCol_Button] = ImVec4{ 0.012f, 0.4f, 0.839f, 1.0f };
             colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
             colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
 
             // Frame BG
-            colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
+            colors[ImGuiCol_FrameBg] = ImVec4{ 0.114f, 0.129f, 0.145f, 1.0f };
             colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
             colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+
+            colors[ImGuiCol_MenuBarBg] = ImVec4{ 0.141f, 0.161f, 0.18f, 1.0f };
 
             // Tabs
             colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
@@ -72,7 +76,7 @@ class RenderGUISystem: public System {
             colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
 
             // Title
-            colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+            colors[ImGuiCol_TitleBg] = ImVec4{ 0.267f, 0.302f, 0.337f, 1.0f };
             colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
             colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
 
